a680b79ed6c132690dbf176937a464a2a419d652
muragesh <muragesh@netprise.co.in>
2021-05-22 11:07:42 +0530

    completed all operations

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const router = express.Router();
  * parse requests of content-type - application/json
  */
 app.use(bodyParser.json());
-/**
- * parse requests of content-type - application/x-www-form-urlencoded
- */
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", require("./routes/user_routes"));
 app.listen(4000);
 console.log("Listening to PORT 8000--");
